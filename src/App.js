11d98151e5c812f07704a8f60f0b4ae6e0c8341f
Marek <marek@mail.cz>
2022-12-19 22:01:00 +0100

    ikona + dalsi podrobnosti

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric&lang=cz`
       );
       const data = await response.json();
       setWeatherData(data);
@@ -26,14 +26,27 @@ function App() {
         </div>
       )
     }
+
+    let iconcode = weatherData.weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    let viditelnostKm = weatherData.visibility/1000;
+
     return (
-      <div>
+      <div className="App">
+        <h2>Město: {weatherData.name}</h2>
         <h2>Teplota: {weatherData.main.temp}°C</h2>
+        <h2>Pocitová teplota: {weatherData.main.feels_like}°C</h2>
         <h3>Popis: {weatherData.weather[0].description}</h3>
+        <div><img src={iconurl} alt="Ikona"></img></div>
+
+        <h4>Viditelnost: {weatherData.visibility} metrů ({viditelnostKm} km)</h4>
+
+
+
       </div>
     );
   } else {
-    return <div>Loading...</div>;
+    return <div><h1>Loading...</h1></div>;
   }
 }
 
